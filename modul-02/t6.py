6992013275c9f5acc8262e2ac63aa1e8a7e48340
KimTun <kim.tunturi@gmail.com>
2025-08-20 22:03:28 +0300

    up

---
+++ modul-02/t6.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+yksi = random.randint(0, 9)
+kaksi = random.randint(0, 9)
+kolme = random.randint(0, 9)
+
+neliyksi = random.randint(1, 6)
+nelikaksi = random.randint(1, 6)
+nelikolme = random.randint(1, 6)
+nelineljä = random.randint(1, 6)
+
+print(str(yksi) + str(kaksi) + str(kolme))
+print(str(neliyksi) + str(nelikaksi) + str(nelikolme) + str(nelineljä) )
