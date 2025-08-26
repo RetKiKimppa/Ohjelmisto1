500f4a09ba596380ee72bdda5482ae152b225ab4
KimTun <kim.tunturi@gmail.com>
2025-08-26 19:08:44 +0300

    up

--- modul-03/t3.py
+++ modul-03/t3.py
@@ -3,17 +3,17 @@ hemoglobiini = float(input("Kerrotko vielä hemoglobiini arvosi? (g/L): "))
 
 
 if sukupuoli == "mies" and 196 > hemoglobiini > 133 :
-    print("normaali taso mihelle")
+    print("normaali taso miehelle")
 elif sukupuoli  == "mies" and hemoglobiini < 134:
-    print("alhainen taso miehlle")
+    print("alhainen taso miehelle")
 elif sukupuoli == "mies" and hemoglobiini > 195:
-    print("korkea taso miehlle")
+    print("korkea taso miehelle")
 
 elif sukupuoli == "nainen" and 176 > hemoglobiini > 116 :
     print("normaali taso naiselle")
-elif sukupuoli  == "nainen" and hemoglobiini < 134:
+elif sukupuoli  == "nainen" and hemoglobiini < 117:
     print("alhainen taso naiselle")
-elif sukupuoli == "nainen" and hemoglobiini > 195:
+elif sukupuoli == "nainen" and hemoglobiini > 175:
     print("korkea taso naiselle")
 else:
     print("Väärä vastaus")
