ccb7cf58ebbc36b5cdfea3501b36146e41cbfbc5
KimTun <kim.tunturi@gmail.com>
2025-08-26 11:32:42 +0300

    up

--- modul-03/tuntiesim.3.py
+++ modul-03/tuntiesim.3.py
@@ -6,7 +6,7 @@ kemia = float(input ("Kerro kemian suorituspisteet: ") )
 
 if fysiikka < 50 or matikka < 50 or kemia < 50:
     print("Et saanut stipendiä")
-elif fysiikka and matikka > 90:
+elif fysiikka > 90 and matikka > 90:
     print("Sait stipendin matematiikan ja fysiikan perusteella")
 elif kemia > 95:
     print("Sait stipendin kemian perusteella")
