976064d5df166999dcce666296e80c32e37e8520
kimtun <kim.tunturi@gmail.com>
2025-08-28 17:45:36 +0300

    up

---
+++ modul-04/t3.py
@@ -0,0 +1,31 @@
+#luku = input("Anna luku: ")
+
+#if luku != "":
+ #   arvo = float(luku)
+  #  suurin = arvo
+   # pienin = arvo
+
+    #while True:
+     #   if float(luku) <= pienin:
+      #      pienin = float(luku)
+       # elif float(luku) >= suurin:
+        #    suurin = float(luku)
+        #luku = input("Anna luku: ")
+#else:
+ #   print(f"Suurin luku: {suurin}, Pienin luku: {pienin}")
+
+
+luku = input("Syötä luku: ")
+
+if luku != "" or str:
+    arvo = float(luku)
+    suurin = arvo
+    pienin = arvo
+
+    while True:
+
+    #kesken
+
+
+
+
