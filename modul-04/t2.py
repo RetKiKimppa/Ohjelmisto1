dcd63087da48035c7d11c9c0b985089dcc20e1b3
KimTun <kim.tunturi@gmail.com>
2025-08-28 11:05:24 +0300

    up

---
+++ modul-04/t2.py
@@ -0,0 +1,7 @@
+tuuma = float(input("Anna tuumaluku: "))
+
+while tuuma >= 0:
+    print(f"{tuuma * 2.54}cm")
+    tuuma = float(input("Anna tuumaluku: "))
+
+print("Toiminnot pysäytetty negatiivisen luvun takia.")
