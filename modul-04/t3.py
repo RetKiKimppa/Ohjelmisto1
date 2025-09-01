479ce24b97d50673ba0bb5e0702dc23bc22e4b49
KimTun <kim.tunturi@gmail.com>
2025-09-01 19:06:01 +0300

    up

--- modul-04/t3.py
+++ modul-04/t3.py
@@ -1,33 +1,19 @@
 luku = input("Anna luku: ")
 
-#if luku != "":
- #   arvo = float(luku)
-    suurin = arvo
-    pienin = arvo
-
-    while luku != "":
-        if float(luku) <= pienin:
-            pienin = float(luku)
-        elif float(luku) >= suurin:
-            suurin = float(luku)
-        luku = input("Anna luku: ")
-
-print(f"Suurin luku: {suurin}, Pienin luku: {pienin}")
-#else:
- #   print(f"Suurin luku: {suurin}, Pienin luku: {pienin}")
-
-
-luku = input("Syötä luku: ")
-
-if luku != "" or str:
+if luku != "":
     arvo = float(luku)
     suurin = arvo
     pienin = arvo
 
     while True:
+        luku = input("Anna luku: ")
+        if luku == "":
+            print(f"Suurin luku: {suurin}, pienin luku: {pienin}")
+            break
+        elif float(luku) > suurin:
+            suurin = float(luku)
+        elif float(luku) < pienin:
+            pienin = float(luku)
 
-    #kesken
-
-
-
-
+elif luku == "":
+    print("Anna edes yksi luku")
