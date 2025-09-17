272e3f0f4e30cbdadb3b319b065707ee9f3e9d0d
KimTun <kim.tunturi@gmail.com>
2025-09-17 20:18:32 +0300

    up

---
+++ modul-08/t1.py
@@ -0,0 +1,27 @@
+import mysql.connector
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='lentokentat',
+         user='kim',
+         password='1111',
+         autocommit=True
+         )
+
+def lentokenttahaku(icao):
+    sql = f"SELECT name, FROM WHERE ident='{icao}'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print(f"Lentokentän nimi: {rivi[0]}")
+    else:
+        print("Syötteelläsi ei löytyny tietoja")
+    return
+
+
+lentokenttahaku("EFHK")
+
+KESKEN
