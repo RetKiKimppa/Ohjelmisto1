8a1528c69ade2cecfff18fd153bd3fb90d8ee099
KimTun <kim.tunturi@gmail.com>
2025-09-19 23:58:51 +0300

    pöytäkone

--- harjoitus/Funtioita.py
+++ harjoitus/Funtioita.py
@@ -2,13 +2,14 @@ def minipeli_trivia():
     kysymykset = [
         ("Mikä on Suomen pääkaupunki: ", "helsinki"),
         ("Kuka on Suomen presidentti: ", "alexander stubb"),
-        ("Monta Metropoliaa on: ", "neljä"),
+        ("Monta Metropoliaa on: ", "4"),
         ("Mitä lehmä juo: ", "vettä"),
         ("Mikä on paras kasino: ", "kasinosetä"),
     ]
     tulos = 0
     max_sakko =  2
     sakko = 0
+
     for kysymys, oikea_vastaus in kysymykset:
         vastaus = input(kysymys).strip().lower()
         if vastaus == oikea_vastaus:
